bbfe6a5f9207ac7b780de8fc51118380c3de324b
WoofWoof0 <shinyeejing@gmail.com>
2026-07-07 22:01:21 -0600

    Phase 7 - Android Rewrite in Kotlin\nTask 7.2 - Camera Access on Android. (checked)

--- android/app/src/main/java/com/example/push_up_counter/MainActivity.kt
+++ android/app/src/main/java/com/example/push_up_counter/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.push_up_counter
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.Manifest
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +24,27 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                100
+            )
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build()
+            val previewView = findViewById<PreviewView>(R.id.previewView)
+            preview.setSurfaceProvider(previewView.surfaceProvider)
+            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+            cameraProvider.bindToLifecycle(this, cameraSelector,preview)}, ContextCompat.getMainExecutor((this)))
     }
 }
