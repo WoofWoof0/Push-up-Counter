4b60618b8baab69ab2a15d986961d412cebc033c
WoofWoof0 <shinyeejing@gmail.com>
2026-07-14 00:14:26 -0600

    Phase 7 - Android Rewrite in Kotlin\nTask 7.5 - Rep counting logic. (checked)

--- android/app/src/main/java/com/example/push_up_counter/MainActivity.kt
+++ android/app/src/main/java/com/example/push_up_counter/MainActivity.kt
@@ -18,20 +18,85 @@ import com.google.mediapipe.framework.image.MediaImageBuilder
 import com.google.mediapipe.tasks.core.BaseOptions
 import com.google.mediapipe.tasks.vision.core.RunningMode
 import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarker
+import kotlin.math.acos
+import kotlin.math.sqrt
+
 class MainActivity : AppCompatActivity() {
     var poseLandmarker: PoseLandmarker? = null
     val modelName = "pose_landmarker_full.task"
     val baseOptionBuilder = BaseOptions.builder().setModelAssetPath(modelName)
     val baseOptions = baseOptionBuilder.build()
 
+
+    // declare the counter variable for the push-up numbers of the user.
+    var counter = 0
+    // declare the current stage of the push-up.
+    var stage: String? = null
+
+    //decalre the threshold of the angles.
+    var upAngle = 160
+    var downAngle = 90
+    private fun calculate(a: FloatArray, b: FloatArray, c: FloatArray): Float {
+
+        val ba = FloatArray(2) {a[it] - b[it]}
+        val bc = FloatArray(2) {c[it] - b[it]}
+
+        val dot = FloatArray(2) {ba[it] * bc[it]}.sum()
+
+        val normBa = sqrt(FloatArray(2){ba[it]*ba[it]}.sum())
+        val normBc = sqrt(FloatArray(2){bc[it]*bc[it]}.sum())
+
+        val cosine = dot / (normBa * normBc)
+        val clipCosine = cosine.coerceIn(-1.0f,1.0f)
+
+        val angleInRadians: Double = acos(clipCosine.toDouble())
+        val angleInDegree: Double = Math.toDegrees(angleInRadians)
+
+        return angleInDegree.toFloat()
+    }
+
     val optionsBuilder = PoseLandmarker.PoseLandmarkerOptions.builder()
         .setBaseOptions(baseOptionBuilder.build())
-        .setResultListener { result, image ->  }
+        .setResultListener { result, image ->
+            val  landmarks = result.landmarks().firstOrNull() ?: return@setResultListener
+            val leftShoulder = landmarks[11]
+            val rightShoulder = landmarks[12]
+
+            val leftElbow = landmarks[13]
+            val rightElbow = landmarks[14]
+
+            val leftWrist = landmarks[15]
+            val rightWrist = landmarks[16]
+
+            val leftShoulderArray = floatArrayOf(leftShoulder.x(),leftShoulder.y())
+            val rightShoulderArray = floatArrayOf(rightShoulder.x(),rightShoulder.y())
+
+            val leftElbowArray = floatArrayOf(leftElbow.x(),leftElbow.y())
+            val rightElbowArray = floatArrayOf(rightElbow.x(),rightElbow.y())
+
+            val leftWristArray = floatArrayOf(leftWrist.x(),leftWrist.y())
+            val rightWristArray = floatArrayOf(rightWrist.x(),rightWrist.y())
+
+            val leftAngle = calculate(leftShoulderArray,leftElbowArray,leftWristArray)
+
+            if (leftAngle >= upAngle) {
+                if (stage == "down") {
+                    counter += 1
+                }
+                stage = "up"
+            } else if (leftAngle <= downAngle) {
+                if (stage == "up"){
+                    stage = "down"
+                }
+            }
+        }
         .setErrorListener {  }
         .setRunningMode(RunningMode.LIVE_STREAM)
 
     val option = optionsBuilder.build()
 
+
+    //put androidx.camera.core.ExperimentalGetImage to suppress the warning prompt.
     @androidx.camera.core.ExperimentalGetImage
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,6 +137,7 @@ class MainActivity : AppCompatActivity() {
             imageAnalyzer.setAnalyzer(ContextCompat.getMainExecutor(this)) { imageProxy ->
                 // get the underlying Android Image
                 val mediaImage= imageProxy.image
+
                 if (mediaImage != null) {
                     // Convert to MediaPipe image using MediaImageBuilder
                     val mpImage = MediaImageBuilder(mediaImage).build()
@@ -83,6 +149,7 @@ class MainActivity : AppCompatActivity() {
                 // Close the ImageProxy to prevent camera freeze
                 imageProxy.close()
             }
+
             cameraProvider.bindToLifecycle(this, cameraSelector,preview, imageAnalyzer)},
             ContextCompat.getMainExecutor((this)))
     }
